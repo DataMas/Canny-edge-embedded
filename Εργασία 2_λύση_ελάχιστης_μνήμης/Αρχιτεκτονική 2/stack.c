b1dad8c6af14986f3735c97c78364d718805d763
Nikolaos Chatzipapas <73334869+nikosnikosa@users.noreply.github.com>
2020-12-27 01:46:47 +0200

    Add files via upload

---
+++ Εργασία 2_λύση_ελάχιστης_μνήμης/Αρχιτεκτονική 2/stack.c
@@ -0,0 +1,26 @@
+#include <rt_misc.h>
+
+ __value_in_regs struct __initial_stackheap __user_initial_stackheap( 
+ unsigned R0, unsigned SP, unsigned R2, unsigned SL){    
+ 
+ struct __initial_stackheap config;     
+
+ //config.heap_limit  = 0x00724B40;     
+
+ //config.stack_limit = 0x00004000;
+ //config.stack_limit = 0x00100000;    
+
+/* works */
+//config.heap_base   = 0x01260000;     
+//config.stack_base  = 0x01460000; 
+
+config.heap_base   = 0x48D8D8;     
+config.stack_base  = 0x48FFE8;   
+
+
+/* factory defaults */
+ //config.heap_base   = 0x00060000;     
+ //config.stack_base  = 0x00080000;    
+ return config;} 
+ 
+ 
